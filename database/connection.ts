aa0cedb5fc67e03df1b464a818fb463443e4f1e4
KeranenEelis10 <keraneneelis10@gmail.com>
2024-11-05 13:07:31 +0200

    Added DB connection and connection test

---
+++ database/connection.ts
@@ -0,0 +1,13 @@
+import {config} from "dotenv";
+import {Pool} from "pg";
+
+config();
+
+export const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  port: Number(process.env.POSTGRES_PORT),
+});
+
