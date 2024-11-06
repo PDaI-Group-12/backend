f3d84b71bd14c69446436e3308a992edbcc0bb84
KeranenEelis10 <keraneneelis10@gmail.com>
2024-11-07 00:25:54 +0200

    Add functions to app.ts

--- app.ts
+++ app.ts
@@ -2,8 +2,10 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import cors from "cors";
 import {indexRouter} from "./routes";
 import {usersRouter} from "./routes/users";
+import { authRouter } from "./routes/auth";
 import {pool} from "./database/connection";
 
 export const app = express();
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.listen(3000, () => {
     console.log(`Example app listening on port ${3000}`)
@@ -27,3 +30,4 @@ app.listen(3000, () => {
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
