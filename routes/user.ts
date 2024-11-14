d9694e5f5249ca6bd0d0ac1bb79c38567d99a658
vima20 <ville_matilainen@outlook.com>
2024-11-14 12:55:29 +0200

    List all employers added.

--- routes/user.ts
+++ routes/user.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import {getUserDataAndSalary,addUserHistory,getUserHistory,paymentRequest} from "../controllers/userController";
+import { getUserDataAndSalary, addUserHistory, getUserHistory, paymentRequest, getAllEmployers } from "../controllers/userController";
 
 export const userRouter = express.Router();
 
+userRouter.get('/listemployers', getAllEmployers);
 userRouter.get('/:userid', getUserDataAndSalary);
 userRouter.post('/history', addUserHistory);
 userRouter.get('/:userid/history', getUserHistory);
 userRouter.get('/paymentrequest/:userid', paymentRequest);
-
 
